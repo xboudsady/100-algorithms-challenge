c1d27ff3648bf4926f34728a3aa7adb6491ffd2b
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-08-04 07:36:21 -0700

    First commit

---
+++ type-script-intro/example.ts
@@ -0,0 +1,8 @@
+// Define data type as you pass in an agrument, here we set the parameters to a 'number' type
+
+function add(num1: number, num2: number): number {
+  // We can assign 'types' to a variable. The 'any' type, we can put strings,number, booleans
+  const example: any[] = ["", 0, true];
+
+  return num1 + num2;
+}
